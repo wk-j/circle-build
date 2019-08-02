1af923e3af76ac9009d06b5c6cf8143f93fd586a
wk-j <somnuk.wk@gmail.com>
2019-08-02 23:12:33 +0700

    Initial commit

---
+++ src/Circle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
