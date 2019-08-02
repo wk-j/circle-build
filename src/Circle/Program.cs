c289fee9bdede405608c4565468446156348bdfa
wk-j <somnuk.wk@gmail.com>
2019-08-03 00:03:30 +0700

    Main

--- src/Circle/Program.cs
+++ src/Circle/Program.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using FluentScheduler;
+using Newtonsoft.Json;
 
 namespace Circle {
     class Program {
+        // @todo Implement main function
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            var a = new {
+                A = 100,
+                B = 200
+            };
+
+            var json = JsonConvert.SerializeObject(a);
+            var registry = new Registry();
         }
     }
 }
